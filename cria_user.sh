13967ed0c988b72e618b23285608ef97551c9ff3
vladmais <vlad-correia@hotmail.com>
2023-03-23 00:47:46 +0000

    Arquivos IaC V.01

---
+++ cria_user.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Criando Usuários do Sistema..."
+
+useradd guest10 -c "Usuário Convidado" -s /bin/bash -m -p $(openssl passwd -1 Senha123)
+passwd guest10 -e
+
+useradd guest11 -c "Usuário Convidado" -s /bin/bash -m -p $(openssl passwd -1 Senha123)
+passwd guest11 -e
+
+useradd guest12 -c "Usuário Convidado" -s /bin/bash -m -p $(openssl passwd -1 Senha123)
+passwd guest12 -e
+
+useradd guest13 -c "Usuário Convidado" -s /bin/bash -m -p $(openssl passwd -1 Senha123)
+passwd guest13 -e
+
+echo "Finalizado!"
